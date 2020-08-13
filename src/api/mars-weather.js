25843e2da44b1dddbf76567358118ca7df65777a
rizky sam pratama <rizkysampratama@gmail.com>
2020-08-13 21:44:52 +0700

    speedlimiter and validate apikey

--- src/api/mars-weather.js
+++ src/api/mars-weather.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const axios = require('axios');
 const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
 
 const limiter = rateLimit({
-  windowMs: 30 * 1000, // 30 seconds
-  max: 30 // limit each IP to 2 requests per windowMs
+    windowMs: 30 * 1000, // 30 seconds
+    max: 30 // limit each IP to 2 requests per windowMs
+});
+
+const speedLimiter = slowDown({
+    windowMs: 30 * 1000, // 30 seconds
+    delayAfter: 1, // allow 1 requests per 30 seconds, then...
+    delayMs: 500 // begin adding 500ms of delay per request above 1:
+    // request # 2 is delayed by  500ms
+    // request # 3 is delayed by 1000ms
+    // request # 4 is delayed by 1500ms
+    // etc.
 });
 
 const router = express.Router();
@@ -12,13 +23,28 @@ const router = express.Router();
 const BASE_URL = `https://api.nasa.gov/insight_weather/?`
 
 let cacheData;
-let cacheTime;
+let cacheTime
 
-router.get('/', limiter, async (req, res, next) => {
+// Collection API key for checking, you maybe store this in db
+// for authenticate user
+const apiKeys = new Map();
+apiKeys.set('12345', true);
+
+router.get('/', limiter, speedLimiter, (req, res, next) => {
+    const apiKey = req.get('X-API-KEY');
+    if (apiKeys.has(apiKey)) {
+        next()
+    }else{
+        const error = new Error('Invalid API KEY');
+        next(error);
+    }
+}, async (req, res, next) => {
     // Memori cache
     if (cacheTime && cacheTime > Date.now() - 30 * 1000) {
         return res.json(cacheData);
     }
+
+    console.log(apiKeys);
 
     try {
         const params = new URLSearchParams({
@@ -30,7 +56,7 @@ router.get('/', limiter, async (req, res, next) => {
         // 1. make a request to the nasa api
         const { data } = await axios.get(`${BASE_URL}${params}`)
         cacheData = data;
-        cacheTime= Date.now();
+        cacheTime = Date.now();
         data.cacheTime = cacheTime;
         // 2. respon to this request with data from nasa api
         return res.json(data);
